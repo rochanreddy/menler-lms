dc98690090d51f6b7e39c798191707a91272617f
Rochan Reddy <ayireddyrochanreddy123@gmail.com>
2026-07-13 06:31:33 +0530

    LMS: clearer book icon for reading lessons

--- client/src/components/LessonIcon.jsx
+++ client/src/components/LessonIcon.jsx
@@ -1,6 +1,6 @@
 // Flat 2D stroke icons for lesson types (no emoji). currentColor + size-driven.
 const SHAPES = {
-  text: <><path d="M4 5.5A1.5 1.5 0 0 1 5.5 4H11v16H5.5A1.5 1.5 0 0 1 4 18.5z" /><path d="M20 5.5A1.5 1.5 0 0 0 18.5 4H13v16h5.5A1.5 1.5 0 0 0 20 18.5z" /></>,
+  text: <><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" /><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" /></>,
   video: <><rect x="3" y="6" width="13" height="12" rx="2" /><path d="m16 10 5-3v10l-5-3z" /></>,
   pdf: <><path d="M14 3H7a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8z" /><path d="M14 3v5h5" /></>,
 };
